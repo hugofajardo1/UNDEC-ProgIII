55df2225828b450fe80aa2450a6d269d3b3b1376
Unknown <hugofajardo1@gmail.com>
2018-04-11 18:44:37 -0300

    Aeropuerto Ciudad y Aerolinea

---
+++ Aeropuerto/src/clases/Aerolinea.java
@@ -0,0 +1,14 @@
+package clases;
+
+public class Aerolinea {
+
+	int idAerolinea;
+	String Nombre;
+	
+	public Aerolinea(int idAerolinea, String nombre) {
+		super();
+		this.idAerolinea = idAerolinea;
+		this.Nombre = nombre;
+	}
+	
+}
--- Aeropuerto/src/clases/Ciudad.java
+++ Aeropuerto/src/clases/Ciudad.java
@@ -2,4 +2,16 @@ package clases;
 
 public class Ciudad {
 
+	public Ciudad(Integer idCiudad, String nombre, String codigoPostal) {
+		super();
+		this.idCiudad = idCiudad;
+		this.Nombre = nombre;
+		this.codigoPostal = codigoPostal;
+	}
+	
+	Integer idCiudad;
+	String Nombre;
+	String codigoPostal;
+	
+	
 }
